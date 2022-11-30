93dd4ec8245ab6f0fcebcdd985583a746e68d267
Arndt <arndt_simon@web.de>
2022-11-30 13:11:35 +0100

    Add requirements.txt and setup.py to make package installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='snowflake',
+    version='1.0.0',
+    packages=find_packages(exclude=['test']),
+    url='https://github.com/arndtSim/DSSSWinterwonderland.git',
+    license='LICENSE',
+    author='ArndtSim',
+    author_email='arndt.simon@fau.de'   
+)
