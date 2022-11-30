152f3c6b7df68da9e5a16b215603b3fb9dfca819
Arndt <arndt_simon@web.de>
2022-11-30 13:27:21 +0100

    Include requirements within setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='snowflake',
     version='1.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(),
+    install_requires=["numpy", "turtles"],
     url='https://github.com/arndtSim/DSSSWinterwonderland.git',
     license='LICENSE',
     author='ArndtSim',
-    author_email='arndt.simon@fau.de'   
+    author_email='arndt.simon@fau.de' 
 )
